9f2bc8b55f2e392c5398d4dd609099c644891f80
Pasturrin GitHub <samir@dio2.cl>
2012-10-31 11:57:39 -0300

    Database connection hanlded in environment

--- config/environments/development.php
+++ config/environments/development.php
@@ -6,7 +6,11 @@
 	$default = array("producto", "index", "");
 	$default = array("producto", "show", "1");
 	*/
-	$default = array("producto", "index", "");
+	$default = array("prueba", "index", "");
+
+	$database = array(
+		"mysql" => array('host' => 'localhost', 'user' => 'root', 'password' => 'password', 'schema' => 'wazpo')
+	);
 
 ?>
 
--- index.php
+++ index.php
@@ -1,33 +1,32 @@
 <?php
 
-require_once 'php-activerecord/ActiveRecord.php';
+	define('DS', DIRECTORY_SEPARATOR);
+	define('PASTURRIN_BASE_DIR', dirname(__FILE__));
+	define('PASTURRIN_LIB_DIR', PASTURRIN_BASE_DIR . DS . 'pasturrin');
+	define('PASTURRIN_APP_DIR', PASTURRIN_BASE_DIR . DS . 'app');
+	define('APP_ROOT', PASTURRIN_BASE_DIR . DS . 'public');
+	define('APP_LIB_DIR', PASTURRIN_BASE_DIR . DS . 'lib');
+	define('PASTURRIN_VENDOR_DIR', PASTURRIN_BASE_DIR . DS . 'vendor');
+	define('BASE_URL', 'http://wazpo.dev');
 
+	require_once 'php-activerecord/ActiveRecord.php';
+	require_once(PASTURRIN_BASE_DIR . DS . 'config' . DS . 'config.php');
 
-ActiveRecord\Config::initialize(function($cfg) {
-	$cfg->set_model_directory('app/models');
-	$cfg->set_connections(array(
-	    'development' => 'mysql://root:@localhost/wazpo'));
-});
+	ActiveRecord\Config::initialize(function($cfg) use ($connections) {
+		$cfg->set_model_directory('app/models');
+		$cfg->set_connections($connections);
+		$cfg->set_default_connection('development');
+	});
 
 
 require_once('twitteroauth/twitteroauth.php');
 
-include_once(dirname(__FILE__) . DIRECTORY_SEPARATOR . 'config' . DIRECTORY_SEPARATOR . 'config.php');
-include_once(dirname(__FILE__) . DIRECTORY_SEPARATOR . 'config' . DIRECTORY_SEPARATOR . 'environment.php');
-include_once(dirname(__FILE__) . DIRECTORY_SEPARATOR . 'dispatch.php');  
 
-define('DS', DIRECTORY_SEPARATOR);
-define('PASTURRIN_BASE_DIR', dirname(__FILE__));
-define('PASTURRIN_LIB_DIR', PASTURRIN_BASE_DIR . DS . 'pasturrin');
-define('PASTURRIN_APP_DIR', PASTURRIN_BASE_DIR . DS . 'app');
-define('APP_ROOT', PASTURRIN_BASE_DIR . DS . 'public');
-define('APP_LIB_DIR', PASTURRIN_BASE_DIR . DS . 'lib');
-define('PASTURRIN_VENDOR_DIR', PASTURRIN_BASE_DIR . DS . 'vendor');
-define('BASE_URL', 'http://wazpo.dev');
+include_once(PASTURRIN_BASE_DIR . DS . 'dispatch.php');  
 
 
-set_include_path(get_include_path() . PATH_SEPARATOR . PASTURRIN_LIB_DIR . PATH_SEPARATOR . PASTURRIN_VENDOR_DIR);
-set_include_path(get_include_path() . PATH_SEPARATOR . PASTURRIN_APP_DIR . PATH_SEPARATOR . 'models');
+//set_include_path(get_include_path() . PATH_SEPARATOR . PASTURRIN_LIB_DIR . PATH_SEPARATOR . PASTURRIN_VENDOR_DIR);
+//set_include_path(get_include_path() . PATH_SEPARATOR . PASTURRIN_APP_DIR . PATH_SEPARATOR . 'models');
 
 function application_autoload($class_name) {
 	if(preg_match('/\w+Controller$/', $class_name)) {
